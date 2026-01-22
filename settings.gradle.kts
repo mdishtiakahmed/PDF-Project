bcb1fb3d660150b99af7b8587e48e5299de647ca
Md Ishtiak Ahmed Robin <149281156+mdishtiakahmed@users.noreply.github.com>
2026-01-22 21:47:19 +0600

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,13 +5,17 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        
+        // 👇 এই লাইনটিই সেই "বিশেষ দোকানের" ঠিকানা
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
-rootProject.name = "RobinBanglaIME"
+rootProject.name = "PDF-Project" // আপনার প্রজেক্টের নাম যদি আলাদা হয়, তবে এটি অটোমেটিক ঠিক থাকবে, হাত দেয়ার দরকার নেই।
 include(":app")
